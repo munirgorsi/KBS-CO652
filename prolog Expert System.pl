e8d17facd9c652f7c5a0e319f6d6ee61ab4a4bd2
mohammadqasimmatloob <Qasimmatloob786@gmail.com>
2024-12-27 17:41:53 +0000

    Greeting
    Double Check the code
    
    Co-Authored-By: Munir Gorsi <115701035+munirgorsi@users.noreply.github.com>

--- prolog Expert System.pl
+++ prolog Expert System.pl
@@ -1,5 +1,11 @@
-prolog
-CW1
-Start to Building a simple Expert System
-testtttt
-tesssy
+% Simple Mortgage Expert System
+
+% Interface name: mortgage_advisor
+
+% Entry point for the expert system
+mortgage_advisor :-
+    write('Well Come to Simple Mortgage Expert System Build By Mohammad Qasim Matloob and Muhammad Munir.'),
+    nl.
+
+% To run the system, consult this file in your Prolog interpreter and execute the 'mortgage_advisor.' command.
+
